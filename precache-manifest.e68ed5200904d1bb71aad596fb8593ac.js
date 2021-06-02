45aad79ff8f96c063bd033614fa289ea045041aa
Koceila2K7 <koceilakireche@gmail.com>
2021-06-02 15:35:14 +0200

    Updates

---
+++ precache-manifest.e68ed5200904d1bb71aad596fb8593ac.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773a02d4e6ef99ebfe71b8b8d4330144",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e57a24634e25071b87a",
+    "url": "/portfolio/static/css/main.b149af82.chunk.css"
+  },
+  {
+    "revision": "49917306e5f84b75d913",
+    "url": "/portfolio/static/js/2.707b074e.chunk.js"
+  },
+  {
+    "revision": "1e57a24634e25071b87a",
+    "url": "/portfolio/static/js/main.4a15e173.chunk.js"
+  },
+  {
+    "revision": "377df5766effcc3f5757",
+    "url": "/portfolio/static/js/runtime-main.f69e773e.js"
+  },
+  {
+    "revision": "3f60edfd3589f7ad589cd03676982f12",
+    "url": "/portfolio/static/media/LinkedIn_logo.3f60edfd.png"
+  },
+  {
+    "revision": "9a174c6f5eced4bc1b629fa1b1c48961",
+    "url": "/portfolio/static/media/Logo_Saclay.9a174c6f.png"
+  },
+  {
+    "revision": "9728a6bdc31bbc765437c0abf850d8ec",
+    "url": "/portfolio/static/media/Logo_Sorbonne_Universite.9728a6bd.png"
+  },
+  {
+    "revision": "9f2b9a6abee7f79baac183fac5e768c6",
+    "url": "/portfolio/static/media/USTHB_Logo.9f2b9a6a.png"
+  },
+  {
+    "revision": "31be4c0b8ca8d104f9920d6b2a914cc4",
+    "url": "/portfolio/static/media/arrow.31be4c0b.svg"
+  },
+  {
+    "revision": "209dcd10075f7628d6cb393e72e4939c",
+    "url": "/portfolio/static/media/beijing.209dcd10.webp"
+  },
+  {
+    "revision": "7a4c228ccf17cbd4a92c7de6104c7eeb",
+    "url": "/portfolio/static/media/dallas.7a4c228c.jpg"
+  },
+  {
+    "revision": "f172be6192d209c75bb977245a08e7de",
+    "url": "/portfolio/static/media/dls.f172be61.png"
+  },
+  {
+    "revision": "9cab3941a9641e93c85fb1b45f2da6a2",
+    "url": "/portfolio/static/media/github_logo.9cab3941.png"
+  },
+  {
+    "revision": "5a5af15ec2d9667537d2b95885262532",
+    "url": "/portfolio/static/media/image2.5a5af15e.jpeg"
+  },
+  {
+    "revision": "ebb8196532e1924ad86530b2cde4c220",
+    "url": "/portfolio/static/media/newyork.ebb81965.webp"
+  },
+  {
+    "revision": "48ac4fa79badd9a3502b83e7d373c0f8",
+    "url": "/portfolio/static/media/paintwars.48ac4fa7.png"
+  },
+  {
+    "revision": "7e07f4ab173bfac805dab28975b1971f",
+    "url": "/portfolio/static/media/pathfinding.7e07f4ab.png"
+  },
+  {
+    "revision": "66f92b7eaa862888afbf25fdf1bdec17",
+    "url": "/portfolio/static/media/pinBoard.66f92b7e.png"
+  },
+  {
+    "revision": "b2931d537103a7991f66a998947b57e6",
+    "url": "/portfolio/static/media/reseau.b2931d53.png"
+  },
+  {
+    "revision": "b58ad59e1fd1f34d49ef692f3ff3ff8d",
+    "url": "/portfolio/static/media/sanfrancisco.b58ad59e.webp"
+  },
+  {
+    "revision": "0c0fccbd0c892c691d84d03653d85e10",
+    "url": "/portfolio/static/media/twisster.0c0fccbd.png"
+  }
+]);
